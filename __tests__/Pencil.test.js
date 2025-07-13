ae6150ec1f1303c922da7db71175e4109ef11f70
MehdiBelghiti <my.mehdi.belghiti@gmail.com>
2025-07-13 12:31:27 +0100

    Test 2: Writing puts text to paper

--- __tests__/Pencil.test.js
+++ __tests__/Pencil.test.js
@@ -1,8 +1,15 @@
-const Pencil = require('../src/Pencil');
+const Pencil = require("../src/Pencil");
 
-describe('Pencil', () => {
-  test('paper starts empty', () => {
+describe("Pencil", () => {
+  test("paper starts empty", () => {
     const pencil = new Pencil();
-    expect(pencil.readPaper()).toBe('');
+    expect(pencil.readPaper()).toBe("");
   });
+
+  test("writing appends text to paper", () => {
+    const pencil = new Pencil();
+    pencil.write("hello");
+    expect(pencil.readPaper()).toBe("hello");
+  });
+
 });
