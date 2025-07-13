af99a09b202900a2eb9d83a1a8fdf18167d8146d
MehdiBelghiti <my.mehdi.belghiti@gmail.com>
2025-07-13 12:49:38 +0100

    Feat 5: Add Type validation for text

--- src/Pencil.js
+++ src/Pencil.js
@@ -3,6 +3,10 @@ class Pencil {
     this.paper = "";
   }
   write(text) {
+    if (typeof text !== "string") {
+      throw new Error("Text must be a string");
+    }
+
     this.paper += text;
   }
   readPaper() {
