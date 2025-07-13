eb787c88f7881b747aae39b0adee5b5822447790
MehdiBelghiti <my.mehdi.belghiti@gmail.com>
2025-07-13 14:32:20 +0100

    Feat 6: lowercase durability implementation

--- src/Pencil.js
+++ src/Pencil.js
@@ -1,14 +1,40 @@
 class Pencil {
-  constructor() {
+  constructor({ durability = 100 } = {}) {
     this.paper = "";
+    this.durability = durability;
   }
+
   write(text) {
     if (typeof text !== "string") {
       throw new Error("Text must be a string");
     }
 
-    this.paper += text;
+    const degradedResult = this.degradePencil(text);
+
+    this.paper += degradedResult;
   }
+
+  degradePencil(text) {
+    let result = "";
+
+    for (const char of text) {
+      if (this.durability <= 0 && /[a-z]/.test(char)) {
+        result += " ";
+      } else {
+        result += char;
+        if (/[a-z]/.test(char)) {
+          this.durability -= 1;
+        }
+      }
+    }
+
+    return result;
+  }
+
+  getDurability() {
+    return this.durability;
+  }
+
   readPaper() {
     return this.paper;
   }
